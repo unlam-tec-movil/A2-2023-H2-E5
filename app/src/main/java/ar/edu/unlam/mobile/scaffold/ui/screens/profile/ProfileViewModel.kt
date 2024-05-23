5d95278c8a2f1d1298d5e1ac8b52d92d12665450
CatramboneMauro <maurojose029@gmail.com>
2024-05-23 16:21:11 -0300

    updateSpacing

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/profile/ProfileViewModel.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/profile/ProfileViewModel.kt
@@ -3,7 +3,4 @@ package ar.edu.unlam.mobile.scaffold.ui.screens.profile
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
-class ProfileViewModel @Inject constructor(
-
-): ViewModel() {
-}
+class ProfileViewModel @Inject constructor() : ViewModel()

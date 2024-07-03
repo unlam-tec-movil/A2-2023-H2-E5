dd492d42d19e1573234844e85a3bfc8247ce5a26
FlorenciaSantamaria98 <fsantamaria@andreani.com>
2024-07-03 16:19:09 -0300

    agrego NutrientsHeader en TrackerOverviewScreen

--- app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/TrackerOverviewScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffold/ui/screens/TrackerOverviewScreen.kt
@@ -19,6 +19,7 @@ import ar.edu.unlam.mobile.scaffold.R
 import ar.edu.unlam.mobile.scaffold.ui.components.AddButton
 import ar.edu.unlam.mobile.scaffold.ui.components.DaySelector
 import ar.edu.unlam.mobile.scaffold.ui.components.ExpandableMeal
+import ar.edu.unlam.mobile.scaffold.ui.components.NutrientsHeader
 import ar.edu.unlam.mobile.scaffold.ui.components.TrackedFoodItem
 import ar.edu.unlam.mobile.scaffold.ui.theme.LocalSpacing
 
@@ -39,6 +40,7 @@ fun TrackerOverviewScreen(
                 .padding(bottom = spacing.spaceMedium),
     ) {
         item {
+            NutrientsHeader(state = state)
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
             DaySelector(
                 date = state.date,
